288b26db88c40df39f81dd326462986ec66b286e
Anuar Alibekov <anuar.alibekov@glidewelldental.com>
2022-04-18 01:01:55 +0600

    Illigal URI to errors

--- app/twoGis/crawler/services/Crawler.scala
+++ app/twoGis/crawler/services/Crawler.scala
@@ -1,11 +1,12 @@
 package twoGis.crawler.services
 
 import net.ruippeixotog.scalascraper.browser.{Browser, JsoupBrowser}
-import play.api.libs.ws.WSClient
+import play.api.libs.ws.{WSClient, WSResponse}
 import twoGis.crawler.models._
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util._
 import scala.util.control.NonFatal
 
 
@@ -17,11 +18,8 @@ class Crawler @Inject()(
 
   def getTitles(urlList: UrlList) ={
     val titles: List[Future[ParsingResult]] = urlList.urls.map(getTitle)
-
     val futTitles = Future.sequence(titles)
-
     val successes = futTitles.map{ l => l.filter{_.isInstanceOf[SuccessfulParsing]}.map(_.asInstanceOf[SuccessfulParsing])}
-
     val failures = futTitles.map{_.filter{_.isInstanceOf[FailedParsing]}.map(_.asInstanceOf[FailedParsing])}
 
     for {
@@ -32,7 +30,13 @@ class Crawler @Inject()(
 
   def getTitle(url: String): Future[ParsingResult] ={
     val req = ws.url(url)
-    req.get().map{ response =>
+
+    val response: Future[WSResponse]  = Try(req.get()) match {
+      case Success(v) => v
+      case Failure(t) => Future.failed(t)
+    }
+
+    response.map{ response =>
       SuccessfulParsing(url, browser.parseString(response.body).title)
     }.recover{
       case NonFatal(t) => FailedParsing(url, t)
